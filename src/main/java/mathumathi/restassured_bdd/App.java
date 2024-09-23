fecd9e18065c407e42816bdc10510dae294040ac
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-09-23 18:50:19 -0400

    draft maven project

---
+++ src/main/java/mathumathi/restassured_bdd/App.java
@@ -0,0 +1,10 @@
+package mathumathi.restassured_bdd;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
